c9f9d9708ae6a3381fb4046efd3f4ce8a21729df
Colin Faris, John Linneman and Zach Spenard <colin@>
2018-03-19 15:30:33 -0700

    add js for cooking calc

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// cooking calculator
+
+var gallonsToLiters = function(gallons) {
+  return (gallons / .26417));
+};
+
+var inputGallons = parseInt(prompt("Enter a number:"));
+var liters = alert(gallonsToLiters(inputGallons);
